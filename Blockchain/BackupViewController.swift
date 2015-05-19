e4dd2e45160b12585401934abb2a5bfe41d48ce9
Sjors Provoost <sjors@sprovoost.nl>
2015-05-19 18:24:40 +0200

    Added backup related strings to Base.lproj/Localizable.strings and en.lproj/Backup.strings (Storyboard).

--- Blockchain/BackupViewController.swift
+++ Blockchain/BackupViewController.swift
@@ -6,11 +6,6 @@
 //  Copyright (c) 2015 Qkos Services Ltd. All rights reserved.
 //
 
-// Strings:
-// "VERIFY BACKUP"
-// "You backed your wallet up %@ days ago"
-// "You only need to backup your wallet once, but it is a good idea to occasionally verify that your backup is valid."
-
 import UIKit
 
 class BackupViewController: UIViewController {
