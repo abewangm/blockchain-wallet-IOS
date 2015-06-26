37d7d0dc729c5c5c6f92532abf3ab33ebbb7aa8e
Mark Pfluger <mark@pfluegerworld.de>
2015-06-26 17:10:39 -0400

    Added BCTextField; minor UI changes

---
+++ Blockchain/BCTextField.swift
@@ -0,0 +1,29 @@
+//
+//  BCTextField.swift
+//  Blockchain
+//
+//  Created by Mark Pfluger on 6/26/15.
+//  Copyright (c) 2015 Qkos Services Ltd. All rights reserved.
+//
+
+import UIKit
+
+class BCTextField: UITextField {
+
+    override func awakeFromNib() {
+        if (self.superview == nil) {
+            return
+        }
+        
+        var onePixelHeight = 1.0/UIScreen.mainScreen().scale
+        var onePixelLine = UIView(frame: CGRectMake(0, self.frame.size.height - onePixelHeight,
+            self.frame.size.width + 20, onePixelHeight))
+        
+        onePixelLine.frame = self.superview!.convertRect(onePixelLine.frame, fromView: self)
+        
+        onePixelLine.userInteractionEnabled = false
+        onePixelLine.backgroundColor = Constants.Colors.TextFieldBorderGray
+        
+        self.superview!.addSubview(onePixelLine)
+    }
+}
---
+++ Blockchain/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Blockchain
+//
+//  Created by Mark Pfluger on 6/26/15.
+//  Copyright (c) 2015 Qkos Services Ltd. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    struct Colors {
+        static let TextFieldBorderGray = UIColorFromRGB(0xcdcdcd)
+    }
+}
+
+// MARK: Helper functions
+
+func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+    return UIColor(
+        red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+        blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+        alpha: CGFloat(1.0)
+    )
+}
