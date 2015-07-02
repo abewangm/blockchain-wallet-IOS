6d983fdac69ece0d99116422cbd5d9ab38d12400
woudini <wukl011@gmail.com>
2015-07-01 22:23:34 -0400

    add upgrade storyboard and connect outlets

---
+++ Blockchain/UpgradeViewController.h
@@ -0,0 +1,13 @@
+//
+//  UpgradeViewController.h
+//  Blockchain
+//
+//  Created by Kevin Wu on 7/1/15.
+//  Copyright (c) 2015 Qkos Services Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpgradeViewController : UIViewController <UIScrollViewDelegate>
+
+@end
---
+++ Blockchain/UpgradeViewController.m
@@ -0,0 +1,24 @@
+//
+//  UpgradeViewController.m
+//  Blockchain
+//
+//  Created by Kevin Wu on 7/1/15.
+//  Copyright (c) 2015 Qkos Services Ltd. All rights reserved.
+//
+
+#import "UpgradeViewController.h"
+
+@interface UpgradeViewController ()
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
+
+@implementation UpgradeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
