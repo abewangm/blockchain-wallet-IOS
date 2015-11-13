9a2e1f9f5f162587fd8673c94e95cbadb1d05066
woudini <wukl011@gmail.com>
2015-11-13 13:47:44 -0500

    remove redundant if condition

--- Blockchain/SettingsTableViewController.m
+++ Blockchain/SettingsTableViewController.m
@@ -734,10 +734,8 @@ const int aboutPrivacyPolicy = 1;
 }
 
 - (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    BOOL hasLoadedAccountInfoDictionary = self.accountInfoDictionary ? YES : NO;
-    
-    if (!hasLoadedAccountInfoDictionary || [[[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULTS_KEY_LOADED_SETTINGS] boolValue] == NO) {
+{    
+    if ([[[NSUserDefaults standardUserDefaults] objectForKey:USER_DEFAULTS_KEY_LOADED_SETTINGS] boolValue] == NO) {
         [self alertUserOfErrorLoadingSettings];
         return nil;
     } else {
