ea9b26d4550979c5922ee444903f01a51cf36534
woudini <wukl011@gmail.com>
2016-02-17 13:46:04 -0500

    catch pairing code error and display refresh instruction

--- Blockchain/LocalizationConstants.h
+++ Blockchain/LocalizationConstants.h
@@ -446,3 +446,4 @@
 #define BC_STRING_NAME_ALREADY_IN_USE NSLocalizedString(@"This name is already in use. Please choose a different name.", nil)
 #define BC_STIRNG_VERIFICATION_EMAIL_SENT_TO_ARGUMENT NSLocalizedString(@"Verification email has been sent to %@.", nil)
 #define BC_STRING_PLEASE_CHECK_AND_CLICK_EMAIL_VERIFICATION_LINK NSLocalizedString(@"Please check your email and click on the verification link.", nil)
+#define BC_STRING_ERROR_PLEASE_REFRESH_PAIRING_CODE NSLocalizedString(@"Please refresh the pairing code and try again.", nil)
