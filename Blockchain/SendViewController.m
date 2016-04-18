4cc10739b854264c5904f686d86364317e6df827
woudini <wukl011@gmail.com>
2016-04-18 15:43:35 -0400

    hide prompt after user opens App Store link

--- Blockchain/SendViewController.m
+++ Blockchain/SendViewController.m
@@ -401,6 +401,7 @@ BOOL displayingLocalSymbolSend;
                      
                      UIAlertController *appReviewAlert = [UIAlertController alertControllerWithTitle:BC_STRING_APP_REVIEW_PROMPT_TITLE message:BC_STRING_APP_REVIEW_PROMPT_MESSAGE preferredStyle:UIAlertControllerStyleAlert];
                      [appReviewAlert addAction:[UIAlertAction actionWithTitle:BC_STRING_YES_RATE_BLOCKCHAIN_WALLET style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                         [[NSUserDefaults standardUserDefaults] setBool:YES forKey:USER_DEFAULTS_KEY_HIDE_APP_REVIEW_PROMPT];
                          [app rateApp];
                      }]];
                      [appReviewAlert addAction:[UIAlertAction actionWithTitle:BC_STRING_ASK_ME_LATER style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
