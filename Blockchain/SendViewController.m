2b5b950f99cef52f16f5089d8a9126bac49d0824
woudini <wukl011@gmail.com>
2016-05-26 09:03:54 -0400

    Revert "fix send screen - keep to and from labels separate"
    This reverts commit 85ac73a3679dc96224de290ed2840118b7f099a9.

--- Blockchain/SendViewController.m
+++ Blockchain/SendViewController.m
@@ -115,15 +115,6 @@ BOOL displayingLocalSymbolSend;
     [self reload];
 }
 
-- (void)viewDidLayoutSubviews
-{
-    [super viewDidLayoutSubviews];
-    
-    if (fromLabel.frame.origin.y == toLabel.frame.origin.y && !fromLabel.hidden) {
-        [self showFromField];
-    }
-}
-
 - (void)resetPayment
 {
     self.surgeIsOccurring = NO;
