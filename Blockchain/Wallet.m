275f8f43b14c67c70aa1b0a6231989d60c7424b7
woudini <wukl011@gmail.com>
2016-09-16 16:01:50 -0400

    Revert "fix(Wallet): avoid extra calls to unspents when creating wallet"
    This reverts commit 6f4b3a1c4033f54ef1f9fc21cdc4622ef6f45b7c.

--- Blockchain/Wallet.m
+++ Blockchain/Wallet.m
@@ -1414,7 +1414,7 @@
 
 - (void)changePaymentFromAccount:(int)fromInt isAdvanced:(BOOL)isAdvanced
 {
-    if (![self isInitialized] || self.isNew) {
+    if (![self isInitialized]) {
         return;
     }
     
@@ -1423,7 +1423,7 @@
 
 - (void)changePaymentFromAddress:(NSString *)fromString isAdvanced:(BOOL)isAdvanced
 {
-    if (![self isInitialized] || self.isNew) {
+    if (![self isInitialized]) {
         return;
     }
     
