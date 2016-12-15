aa09a6c0e8df92b11464293ca711ba4262987adc
woudini <wukl011@gmail.com>
2016-12-15 10:26:48 -0500

    fix(Contacts): use production URLs

--- Blockchain/js/wallet-ios.js
+++ Blockchain/js/wallet-ios.js
@@ -28,7 +28,7 @@ min = false;
 // Set the API code for the iOS Wallet for the server calls
 //BlockchainAPI.API_CODE = API_CODE;
 BlockchainAPI.AJAX_TIMEOUT = 30000; // 30 seconds
-BlockchainAPI.API_ROOT_URL = 'https://api.dev.blockchain.info/'
+BlockchainAPI.API_ROOT_URL = 'https://api.blockchain.info/'
 
 var MyWalletPhone = {};
 var currentPayment = null;
