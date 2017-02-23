a50ad6c6f0ef25df05e447441ac04afe95b71735
woudini <wukl011@gmail.com>
2017-02-23 13:53:50 -0500

    fix(RootService): fix auto pairing

--- Blockchain/RootService.m
+++ Blockchain/RootService.m
@@ -1399,17 +1399,20 @@ void (^secondPasswordSuccess)(NSString *);
     PairingCodeParser * pairingCodeParser = [[PairingCodeParser alloc] initWithSuccess:^(NSDictionary*code) {
         DLog(@"scanAndParse success");
         
-        [app forgetWallet];
-        
-        [app clearPin];
-        
-        [self.wallet loadWalletWithGuid:[code objectForKey:QR_CODE_KEY_GUID] sharedKey:[code objectForKey:QR_CODE_KEY_SHARED_KEY] password:[code objectForKey:QR_CODE_KEY_PASSWORD]];
-        
-        self.wallet.delegate = self;
-        
-        wallet.didPairAutomatically = YES;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [app forgetWallet];
+            
+            [app clearPin];
+            
+            self.wallet.delegate = self;
+            
+            [self.wallet loadWalletWithGuid:[code objectForKey:QR_CODE_KEY_GUID] sharedKey:[code objectForKey:QR_CODE_KEY_SHARED_KEY] password:[code objectForKey:QR_CODE_KEY_PASSWORD]];
+            
+            wallet.didPairAutomatically = YES;
+        });
         
     } error:^(NSString*error) {
+        [app hideBusyView];
         [app standardNotify:error];
     }];
     
