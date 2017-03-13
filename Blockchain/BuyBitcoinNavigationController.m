fbc60922510ca9590a44162006171715a701201e
woudini <wukl011@gmail.com>
2017-03-13 12:09:44 -0400

    fix(Buy): use enums to handle UIImagePickerController in BCNavigationController subclass

--- Blockchain/BuyBitcoinNavigationController.m
+++ Blockchain/BuyBitcoinNavigationController.m
@@ -8,23 +8,46 @@
 
 #import "BuyBitcoinNavigationController.h"
 
+typedef enum {
+    DismissStateDefault,
+    DismissStateWillPreventExtraDismiss,
+    DismissStateIsPreventingExtraDismiss,
+}DismissState;
+
 @interface BuyBitcoinNavigationController ()
-@property (nonatomic) BOOL willAttemptDismissTwice;
+@property (nonatomic) DismissState dismissState;
 @end
 
 @implementation BuyBitcoinNavigationController
 
-- (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion
+- (void)presentViewController:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion
 {
+    [super presentViewController:viewControllerToPresent animated:flag completion:completion];
+    
     // http://stackoverflow.com/questions/35999551/uiimagepickercontroller-view-is-not-in-the-window-hierarchy
     
-    BOOL willAttemptDismissTwice = [self.presentedViewController isMemberOfClass:[UIImagePickerController class]];
-    
-    if (!self.willAttemptDismissTwice) {
+    self.dismissState = [viewControllerToPresent isMemberOfClass:[UIDocumentMenuViewController class]] ? DismissStateWillPreventExtraDismiss : DismissStateDefault;
+}
+
+- (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion
+{
+    if (self.dismissState == DismissStateWillPreventExtraDismiss) {
+        if ([self.presentedViewController isMemberOfClass:[UIDocumentMenuViewController class]]) {
+            // User selected an action - dismissViewControllerAnimated:completion: will be called an extra time, but the first time must be allowed otherwise the UIDocumentMenuViewController will not dismiss at all.
+            [super dismissViewControllerAnimated:flag completion:completion];
+            self.dismissState = DismissStateIsPreventingExtraDismiss;
+            return;
+        } else {
+            // User selected cancel - UIDocumentMenuViewController has been dismissed from a separate dismiss method, but dismissViewControllerAnimated:completion: will be called an extra time.
+            self.dismissState = DismissStateIsPreventingExtraDismiss;
+            return;
+        }
+    } else if (self.dismissState == DismissStateIsPreventingExtraDismiss) {
+        self.dismissState = DismissStateDefault;
+        return;
+    } else {
         [super dismissViewControllerAnimated:flag completion:completion];
     }
-    
-    self.willAttemptDismissTwice = willAttemptDismissTwice;
 }
 
 @end
