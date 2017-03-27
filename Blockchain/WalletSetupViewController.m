6f0594bc9de1e29ee57d3fa8178d6044a55ec78b
woudini <wukl011@gmail.com>
2017-03-27 16:17:38 -0400

    prototype(Onboarding): add actions to go to second page and dismiss

--- Blockchain/WalletSetupViewController.m
+++ Blockchain/WalletSetupViewController.m
@@ -9,7 +9,7 @@
 #import "WalletSetupViewController.h"
 
 @interface WalletSetupViewController ()
-
+@property (nonatomic) UIScrollView *scrollView;
 @end
 
 @implementation WalletSetupViewController
@@ -41,27 +41,49 @@
     
     NSInteger numberOfPages = 2;
     
+    [scrollView addSubview:[self setupTouchIDView]];
+    [scrollView addSubview:[self setupEmailView]];
+    
+    scrollView.contentSize = CGSizeMake(self.view.frame.size.width * numberOfPages, self.view.frame.size.height);
+    [self.view addSubview:scrollView];
+    
+    self.scrollView = scrollView;
+}
+
+- (UIView *)setupTouchIDView
+{
     UIView *touchIDView = [[UIView alloc] initWithFrame:self.view.frame];
     touchIDView.backgroundColor = [UIColor clearColor];
     UIButton *doneButtonTouchID = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width - 40, 30)];
     doneButtonTouchID.center = CGPointMake(self.view.frame.size.width/2, doneButtonTouchID.center.y);
     [doneButtonTouchID setTitleColor:COLOR_LIGHT_GRAY forState:UIControlStateNormal];
     [doneButtonTouchID setTitle:BC_STRING_ILL_DO_THIS_LATER forState:UIControlStateNormal];
+    [doneButtonTouchID addTarget:self action:@selector(goToSecondPage) forControlEvents:UIControlEventTouchUpInside];
     [touchIDView addSubview:doneButtonTouchID];
-    
+    return touchIDView;
+}
+
+- (UIView *)setupEmailView
+{
     UIView *emailView = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height)];
     emailView.backgroundColor = [UIColor clearColor];
     UIButton *doneButtonEmail = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width - 40, 30)];
     doneButtonEmail.center = CGPointMake(self.view.frame.size.width/2, doneButtonEmail.center.y);
     [doneButtonEmail setTitleColor:COLOR_LIGHT_GRAY forState:UIControlStateNormal];
     [doneButtonEmail setTitle:BC_STRING_ILL_DO_THIS_LATER forState:UIControlStateNormal];
+    [doneButtonEmail addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
     [emailView addSubview:doneButtonEmail];
-    
-    [scrollView addSubview:touchIDView];
-    [scrollView addSubview:emailView];
-    
-    scrollView.contentSize = CGSizeMake(self.view.frame.size.width * numberOfPages, self.view.frame.size.height);
-    [self.view addSubview:scrollView];
+    return emailView;
+}
+
+- (void)goToSecondPage
+{
+    [self.scrollView setContentOffset:CGPointMake(self.view.frame.size.width, 0) animated:YES];
+}
+
+- (void)dismiss
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
