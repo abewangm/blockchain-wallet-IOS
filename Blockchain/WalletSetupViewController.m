b646073440fbdd3ef7b35194a349e97231216ba9
woudini <wukl011@gmail.com>
2017-03-27 17:16:07 -0400

    prototype(Onboarding): use separate methods to create buttons

--- Blockchain/WalletSetupViewController.m
+++ Blockchain/WalletSetupViewController.m
@@ -54,12 +54,16 @@
 {
     UIView *touchIDView = [[UIView alloc] initWithFrame:self.view.frame];
     touchIDView.backgroundColor = [UIColor clearColor];
-    UIButton *doneButtonTouchID = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width - 40, 30)];
-    doneButtonTouchID.center = CGPointMake(self.view.frame.size.width/2, doneButtonTouchID.center.y);
-    [doneButtonTouchID setTitleColor:COLOR_LIGHT_GRAY forState:UIControlStateNormal];
-    [doneButtonTouchID setTitle:BC_STRING_ILL_DO_THIS_LATER forState:UIControlStateNormal];
-    [doneButtonTouchID addTarget:self action:@selector(goToSecondPage) forControlEvents:UIControlEventTouchUpInside];
-    [touchIDView addSubview:doneButtonTouchID];
+    
+    UIButton *enableTouchIDButton = [self setupActionButton];
+    [enableTouchIDButton setTitle:BC_STRING_ENABLE_TOUCH_ID forState:UIControlStateNormal];
+    [enableTouchIDButton addTarget:self action:@selector(enableTouchID) forControlEvents:UIControlEventTouchUpInside];
+    [touchIDView addSubview:enableTouchIDButton];
+    
+    UIButton *doneButton = [self setupDoneButton];
+    [doneButton addTarget:self action:@selector(goToSecondPage) forControlEvents:UIControlEventTouchUpInside];
+    [touchIDView addSubview:doneButton];
+    
     return touchIDView;
 }
 
@@ -67,13 +71,37 @@
 {
     UIView *emailView = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.size.width, 0, self.view.frame.size.width, self.view.frame.size.height)];
     emailView.backgroundColor = [UIColor clearColor];
-    UIButton *doneButtonEmail = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width - 40, 30)];
-    doneButtonEmail.center = CGPointMake(self.view.frame.size.width/2, doneButtonEmail.center.y);
-    [doneButtonEmail setTitleColor:COLOR_LIGHT_GRAY forState:UIControlStateNormal];
-    [doneButtonEmail setTitle:BC_STRING_ILL_DO_THIS_LATER forState:UIControlStateNormal];
-    [doneButtonEmail addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
-    [emailView addSubview:doneButtonEmail];
+    
+    UIButton *openMailButton = [self setupActionButton];
+    [openMailButton setTitle:BC_STRING_OPEN_MAIL_APP forState:UIControlStateNormal];
+    [openMailButton addTarget:self action:@selector(openMail) forControlEvents:UIControlEventTouchUpInside];
+    [emailView addSubview:openMailButton];
+    
+    UIButton *doneButton = [self setupDoneButton];
+    [doneButton addTarget:self action:@selector(dismiss) forControlEvents:UIControlEventTouchUpInside];
+    [emailView addSubview:doneButton];
+    
     return emailView;
+}
+
+- (UIButton *)setupActionButton
+{
+    UIButton *actionButton = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 60 - 30 - 16, self.view.frame.size.width - 40, 40)];
+    actionButton.titleLabel.font = [UIFont fontWithName:FONT_MONTSERRAT_REGULAR size:14];
+    actionButton.center = CGPointMake(self.view.frame.size.width/2, actionButton.center.y);
+    actionButton.backgroundColor = COLOR_BLOCKCHAIN_LIGHT_BLUE;
+    [actionButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    return actionButton;
+}
+
+- (UIButton *)setupDoneButton
+{
+    UIButton *doneButton = [[UIButton alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 60, self.view.frame.size.width - 40, 40)];
+    doneButton.titleLabel.font = [UIFont fontWithName:FONT_MONTSERRAT_REGULAR size:14];
+    doneButton.center = CGPointMake(self.view.frame.size.width/2, doneButton.center.y);
+    [doneButton setTitleColor:COLOR_LIGHT_GRAY forState:UIControlStateNormal];
+    [doneButton setTitle:BC_STRING_ILL_DO_THIS_LATER forState:UIControlStateNormal];
+    return doneButton;
 }
 
 - (void)goToSecondPage
@@ -85,6 +113,16 @@
 {
     self.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)openMail
+{
+    
+}
+
+- (void)enableTouchID
+{
+    
 }
 
 @end
