74d5e25574a6e2b51edfb407f988015b80f52d10
woudini <wukl011@gmail.com>
2017-05-02 16:06:18 -0400

    fix(Style): center header labels and use correct colors for merchant views

--- Blockchain/MerchantMapViewController.m
+++ Blockchain/MerchantMapViewController.m
@@ -77,6 +77,7 @@
     headerLabel.textAlignment = NSTextAlignmentCenter;
     headerLabel.adjustsFontSizeToFitWidth = YES;
     headerLabel.text = BC_STRING_MERCHANT_MAP;
+    headerLabel.center = CGPointMake(topBarView.center.x, headerLabel.center.y);
     [topBarView addSubview:headerLabel];
     
     UIButton *closeButton = [[UIButton alloc] initWithFrame:CGRectMake(self.view.frame.size.width - 80, 15, 80, 51)];
