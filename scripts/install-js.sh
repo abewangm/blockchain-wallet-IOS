1104ec6ff8b0c90f48ea17cfe971901faf4bcc10
woudini <wukl011@gmail.com>
2017-05-31 12:10:08 -0400

    update(Submodules): use v3.32.0 of My-Wallet-V3 and update install script

--- scripts/install-js.sh
+++ scripts/install-js.sh
@@ -7,7 +7,7 @@ echo "Cleaning node_modules..."
 rm -rf node_modules
 
 echo "Installing node_modules..."
-make node_modules/sjcl/sjcl.js
+make node_modules
 
 echo "Installing Grunt plugins..."
 npm install grunt@1.0.1 grunt-browserify@5.0.0 grunt-text-replace@0.4.0 babelify@7.3.0 babel-preset-es2015@6.24.1 babel-plugin-transform-object-assign@6.22.0
