6ef8739e3b8c1d05d78db351b695694b1966ef71
woudini <wukl011@gmail.com>
2017-10-27 14:18:24 -0400

    feat(Scripts): check OS

--- scripts/app-store-build.sh
+++ scripts/app-store-build.sh
@@ -1,6 +1,21 @@
 set -e
 git checkout app-store
 git submodule update --init
+
+echo 'checking OS'
+if [ "$(uname)" == "Darwin" ]; then
+    echo 'Mac OS X'
+    sudo npm install -g n
+    sudo n 7.9.0
+    npm i -g yarn@0.22.0
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+fi
+
 sh scripts/install-js.sh
 sh scripts/build-js.sh
 cd Submodules/OpenSSL-for-iPhone/
