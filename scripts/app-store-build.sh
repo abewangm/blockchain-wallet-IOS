79c56d365c7f7c06c810862904bf50d11329876c
woudini <wukl011@gmail.com>
2017-10-27 14:26:24 -0400

    feat(Scripts): add logs before installing node and yarn for mac

--- scripts/app-store-build.sh
+++ scripts/app-store-build.sh
@@ -5,15 +5,17 @@ git submodule update --init
 echo 'checking OS'
 if [ "$(uname)" == "Darwin" ]; then
     echo 'Mac OS X'
+    echo 'Installing node...'
     sudo npm install -g n
     sudo n 7.9.0
+    echo 'Installing yarn...'
     npm i -g yarn@0.22.0
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under GNU/Linux platform
+    echo 'GNU/Linux'
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    # Do something under 32 bits Windows NT platform
+    echo 'Windows 32 bit'
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # Do something under 64 bits Windows NT platform
+    echo 'Windows 64 bit'
 fi
 
 sh scripts/install-js.sh
