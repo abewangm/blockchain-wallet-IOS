6a47f7345aea5bf10299b44e115664f9607d84ae
woudini <wukl011@gmail.com>
2017-10-30 09:48:16 -0400

    feat(Scripts): show error and exit if not using Mac OS X

--- scripts/app-store-build.sh
+++ scripts/app-store-build.sh
@@ -3,6 +3,7 @@ git checkout app-store
 git submodule update --init
 
 echo 'checking OS'
+OSCheckError='Please run this script on Mac OS X.'
 if [ "$(uname)" == "Darwin" ]; then
     echo 'Mac OS X'
     echo 'Installing node...'
@@ -11,11 +12,17 @@ if [ "$(uname)" == "Darwin" ]; then
     echo 'Installing yarn...'
     npm i -g yarn@0.22.0
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo 'GNU/Linux'
+    echo 'You are using GNU/Linux.'
+    echo $OSCheckError
+    exit 1
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    echo 'Windows 32 bit'
+    echo 'You are using Windows 32 bit.'
+    echo $OSCheckError
+    exit 1
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    echo 'Windows 64 bit'
+    echo 'You are using Windows 64 bit.'
+    echo $OSCheckError
+    exit 1
 fi
 
 sh scripts/install-js.sh
