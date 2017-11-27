fc89b31639eaa1ae0e92b3dbdb65a3859e6aae38
Maurice Achtenhagen <maurice@blockchain.com>
2017-11-27 12:42:25 -0500

    feat(Exchange): set order ID and transaction fee

--- Blockchain/ExchangeTrade.m
+++ Blockchain/ExchangeTrade.m
@@ -11,8 +11,10 @@
 #define DICTIONARY_KEY_STATUS @"status"
 #define DICTIONARY_KEY_PAIR @"pair"
 #define DICTIONARY_KEY_QUOTE @"quote"
+#define DICTIONARY_KEY_ORDER_ID @"orderId"
 #define DICTIONARY_KEY_WITHDRAWAL_AMOUNT @"withdrawalAmount"
 #define DICTIONARY_KEY_DEPOSIT_AMOUNT @"depositAmount"
+#define DICTIONARY_KEY_MINER_FEE @"minerFee"
 
 @implementation ExchangeTrade
 
@@ -23,9 +25,11 @@
     trade.status = [dict objectForKey:DICTIONARY_KEY_STATUS];
     
     NSDictionary *quote = [dict objectForKey:DICTIONARY_KEY_QUOTE];
+    trade.orderID = [quote objectForKey:DICTIONARY_KEY_ORDER_ID];
     trade.pair = [quote objectForKey:DICTIONARY_KEY_PAIR];
     trade.depositAmount = [[NSDecimalNumber alloc] initWithDecimal:[[quote objectForKey:DICTIONARY_KEY_DEPOSIT_AMOUNT] decimalValue]];
     trade.withdrawalAmount = [quote objectForKey:DICTIONARY_KEY_WITHDRAWAL_AMOUNT];
+    trade.transactionFee = [quote objectForKey:DICTIONARY_KEY_MINER_FEE];
     return trade;
 }
 
