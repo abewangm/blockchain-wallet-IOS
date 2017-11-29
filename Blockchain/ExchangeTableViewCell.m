72d8417d5061feb869f7726ddbe697ad9039a8d0
woudini <10579422+woudini@users.noreply.github.com>
2017-11-29 16:41:49 -0500

    feat(Exchange): add known trade statuses

--- Blockchain/ExchangeTableViewCell.m
+++ Blockchain/ExchangeTableViewCell.m
@@ -9,31 +9,29 @@
 #import "ExchangeTableViewCell.h"
 #import "NSDateFormatter+TimeAgoString.h"
 
-#define STATUS_COMPLETE @"complete"
-#define STATUS_IN_PROGRESS @"inprogress"
-#define STATUS_CANCELLED @"cancelled"
-#define STATUS_FAILED @"failed"
-#define STATUS_EXPIRED @"expired"
-
 @implementation ExchangeTableViewCell
 
 - (void)configureWithTrade:(ExchangeTrade *)trade
 {
     NSString *status = trade.status;
     
+    NSString *displayStatus;
     UIColor *statusColor;
-    if ([status isEqualToString:STATUS_COMPLETE]) {
+    if ([status isEqualToString:TRADE_STATUS_COMPLETE]) {
         statusColor = COLOR_BLOCKCHAIN_GREEN;
-    } else if ([status isEqualToString:STATUS_IN_PROGRESS]) {
+    } else if ([status isEqualToString:TRADE_STATUS_NO_DEPOSITS] ||
+               [status isEqualToString:TRADE_STATUS_RECEIVED]) {
         statusColor = COLOR_BLOCKCHAIN_GRAY_BLUE;
-    } else if ([status isEqualToString:STATUS_CANCELLED] ||
-               [status isEqualToString:STATUS_FAILED] ||
-               [status isEqualToString:STATUS_EXPIRED]) {
+        displayStatus = BC_STRING_IN_PROGRESS;
+    } else if ([status isEqualToString:TRADE_STATUS_CANCELLED] ||
+               [status isEqualToString:TRADE_STATUS_FAILED] ||
+               [status isEqualToString:TRADE_STATUS_EXPIRED]) {
         statusColor = COLOR_BLOCKCHAIN_RED;
     }
     
     self.actionLabel.textColor = statusColor;
-    self.actionLabel.text = [status uppercaseString];
+    
+    self.actionLabel.text = [displayStatus uppercaseString];
     self.amountButton.backgroundColor = statusColor;
     [self.amountButton setTitle:[trade.withdrawalAmount stringValue] forState:UIControlStateNormal];
     self.dateLabel.text = [NSDateFormatter timeAgoStringFromDate:trade.date];
