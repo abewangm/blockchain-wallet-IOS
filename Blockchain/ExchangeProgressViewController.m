9ac03d3289ba04c50f9f35b500184b07334e454f
woudini <10579422+woudini@users.noreply.github.com>
2017-12-02 16:47:56 -0500

    feat(Exchange): add summary view with height that adjusts to screen size

--- Blockchain/ExchangeProgressViewController.m
+++ Blockchain/ExchangeProgressViewController.m
@@ -8,6 +8,7 @@
 
 #import "ExchangeProgressViewController.h"
 #import "ExchangeDetailView.h"
+#import "UIView+ChangeFrameAttribute.h"
 
 @interface ExchangeProgressViewController ()
 
@@ -19,7 +20,16 @@
 {
     [super viewDidLoad];
     self.view.backgroundColor = COLOR_TABLE_VIEW_BACKGROUND_LIGHT_GRAY;
-    ExchangeDetailView *detailView = [[ExchangeDetailView alloc] initWithFrame:CGRectMake(0, DEFAULT_HEADER_HEIGHT, WINDOW_WIDTH, 0) fetchedTrade:self.trade];
+    
+    ExchangeDetailView *detailView = [[ExchangeDetailView alloc] initWithFrame:CGRectMake(0, 0, WINDOW_WIDTH, 0) fetchedTrade:self.trade];
+    
+    CGFloat windowWidth = WINDOW_WIDTH;
+    BOOL isUsingLargerScreen = IS_USING_SCREEN_SIZE_LARGER_THAN_5S;
+    UIView *summaryView = [[UIView alloc] initWithFrame:CGRectMake(0, DEFAULT_HEADER_HEIGHT, windowWidth, isUsingLargerScreen ? 250 : self.view.frame.size.height - detailView.frame.size.height - 24 - DEFAULT_HEADER_HEIGHT)];
+    [self.view addSubview:summaryView];
+    
+    [detailView changeYPosition:summaryView.frame.origin.y + summaryView.frame.size.height + 16];
+    
     [self.view addSubview:detailView];
 }
 
