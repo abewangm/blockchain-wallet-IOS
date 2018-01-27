d62d627cdb7c751f1cef2b298de8ee49b70afe75
woudini <10579422+woudini@users.noreply.github.com>
2018-01-27 10:56:27 -0500

    fix(Exchange): allow prioritized label to increase in width up to leeway limit

--- Blockchain/ExchangeDetailView.m
+++ Blockchain/ExchangeDetailView.m
@@ -55,19 +55,19 @@
     NSString *withdrawAmount = [NSString stringWithFormat:@"%@ %@", [NSNumberFormatter localFormattedString:[trade.withdrawalAmount stringValue]], withdrawalCurrencySymbol];
     NSString *minerFee = [NSString stringWithFormat:@"%@ %@", [NSNumberFormatter localFormattedString:[trade.minerFee stringValue]], withdrawalCurrencySymbol];
 
-    UIView *rowDeposit = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_DEPOSIT, depositCurrency] accessoryText:depositAmount yPosition:0];
+    UIView *rowDeposit = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_DEPOSIT, depositCurrency] accessoryText:depositAmount yPosition:0 accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowDeposit];
 
-    UIView *rowReceive = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_BE_RECEIVED, receiveCurrency] accessoryText:withdrawAmount yPosition:rowDeposit.frame.origin.y + rowDeposit.frame.size.height];
+    UIView *rowReceive = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_BE_RECEIVED, receiveCurrency] accessoryText:withdrawAmount yPosition:rowDeposit.frame.origin.y + rowDeposit.frame.size.height accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowReceive];
 
-    UIView *rowExchangeRate = [self rowViewWithText:BC_STRING_EXCHANGE_RATE accessoryText:trade.exchangeRateString yPosition:rowReceive.frame.origin.y + rowReceive.frame.size.height];
+    UIView *rowExchangeRate = [self rowViewWithText:BC_STRING_EXCHANGE_RATE accessoryText:trade.exchangeRateString yPosition:rowReceive.frame.origin.y + rowReceive.frame.size.height accessoryLabelWidthHasPriority:YES];
     [self addSubview:rowExchangeRate];
 
-    UIView *rowTransactionFee = [self rowViewWithText:BC_STRING_TRANSACTION_FEE accessoryText:minerFee yPosition:rowExchangeRate.frame.origin.y + rowExchangeRate.frame.size.height];
+    UIView *rowTransactionFee = [self rowViewWithText:BC_STRING_TRANSACTION_FEE accessoryText:minerFee yPosition:rowExchangeRate.frame.origin.y + rowExchangeRate.frame.size.height accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowTransactionFee];
 
-    UIView *rowOrderID = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_EXCHANGE_ORDER_ID, @""] accessoryText:trade.orderID yPosition:rowTransactionFee.frame.origin.y + rowTransactionFee.frame.size.height];
+    UIView *rowOrderID = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_EXCHANGE_ORDER_ID, @""] accessoryText:trade.orderID yPosition:rowTransactionFee.frame.origin.y + rowTransactionFee.frame.size.height accessoryLabelWidthHasPriority:NO];
     self.orderID = trade.orderID;
     UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] init];
     [tapGestureRecognizer addTarget:self action:@selector(orderIDTapped)];
@@ -75,7 +75,7 @@
     rowOrderID.userInteractionEnabled = YES;
     [self addSubview:rowOrderID];
     
-    UIView *rowDate = [self rowViewWithText:BC_STRING_DATE accessoryText:[NSDateFormatter verboseStringFromDate:trade.date] yPosition:rowOrderID.frame.origin.y + rowOrderID.frame.size.height];
+    UIView *rowDate = [self rowViewWithText:BC_STRING_DATE accessoryText:[NSDateFormatter verboseStringFromDate:trade.date] yPosition:rowOrderID.frame.origin.y + rowOrderID.frame.size.height accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowDate];
 
     BCLine *bottomLine = [[BCLine alloc] initWithYPosition:rowDate.frame.origin.y + rowDate.frame.size.height];
@@ -95,42 +95,71 @@
     NSString *minerFee = [NSString stringWithFormat:@"%@ %@", [NSNumberFormatter localFormattedString:[trade.minerFee stringValue]], withdrawalCurrencySymbol];
     NSString *totalSpent = [NSString stringWithFormat:@"%@ %@", [NSNumberFormatter localFormattedString:[[trade.depositAmount decimalNumberByAdding:trade.transactionFee] stringValue]], depositCurrencySymbol];
     
-    UIView *rowDeposit = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_DEPOSIT, depositCurrency] accessoryText:depositAmount yPosition:0];
+    UIView *rowDeposit = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_DEPOSIT, depositCurrency] accessoryText:depositAmount yPosition:0 accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowDeposit];
 
-    UIView *rowTransactionFee = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_TRANSACTION_FEE, depositCurrency] accessoryText:transactionFee yPosition:rowDeposit.frame.origin.y + rowDeposit.frame.size.height];
+    UIView *rowTransactionFee = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_TRANSACTION_FEE, depositCurrency] accessoryText:transactionFee yPosition:rowDeposit.frame.origin.y + rowDeposit.frame.size.height accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowTransactionFee];
     
-    UIView *rowTotalSpent = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_TOTAL_ARGUMENT_SPENT, depositCurrency] accessoryText:totalSpent yPosition:rowTransactionFee.frame.origin.y + rowTransactionFee.frame.size.height];
+    UIView *rowTotalSpent = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_TOTAL_ARGUMENT_SPENT, depositCurrency] accessoryText:totalSpent yPosition:rowTransactionFee.frame.origin.y + rowTransactionFee.frame.size.height accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowTotalSpent];
     
-    UIView *rowExchangeRate = [self rowViewWithText:BC_STRING_EXCHANGE_RATE accessoryText:trade.exchangeRateString yPosition:rowTotalSpent.frame.origin.y + rowTotalSpent.frame.size.height];
+    UIView *rowExchangeRate = [self rowViewWithText:BC_STRING_EXCHANGE_RATE accessoryText:trade.exchangeRateString yPosition:rowTotalSpent.frame.origin.y + rowTotalSpent.frame.size.height accessoryLabelWidthHasPriority:YES];
     [self addSubview:rowExchangeRate];
     
-    UIView *rowNetworkTransactionFee = [self rowViewWithText:BC_STRING_NETWORK_TRANSACTION_FEE accessoryText:minerFee yPosition:rowExchangeRate.frame.origin.y + rowExchangeRate.frame.size.height];
+    UIView *rowNetworkTransactionFee = [self rowViewWithText:BC_STRING_NETWORK_TRANSACTION_FEE accessoryText:minerFee yPosition:rowExchangeRate.frame.origin.y + rowExchangeRate.frame.size.height accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowNetworkTransactionFee];
     
-    UIView *rowReceive = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_BE_RECEIVED, receiveCurrency] accessoryText:withdrawAmount yPosition:rowNetworkTransactionFee.frame.origin.y + rowNetworkTransactionFee.frame.size.height];
+    UIView *rowReceive = [self rowViewWithText:[NSString stringWithFormat:BC_STRING_ARGUMENT_TO_BE_RECEIVED, receiveCurrency] accessoryText:withdrawAmount yPosition:rowNetworkTransactionFee.frame.origin.y + rowNetworkTransactionFee.frame.size.height accessoryLabelWidthHasPriority:NO];
     [self addSubview:rowReceive];
     
     BCLine *bottomLine = [[BCLine alloc] initWithYPosition:rowReceive.frame.origin.y + rowReceive.frame.size.height];
     [self addSubview:bottomLine];
 }
 
-- (UIView *)rowViewWithText:(NSString *)text accessoryText:(NSString *)accessoryText yPosition:(CGFloat)posY
+- (UIView *)rowViewWithText:(NSString *)text accessoryText:(NSString *)accessoryText yPosition:(CGFloat)posY accessoryLabelWidthHasPriority:(BOOL)accessoryLabelWidthHasPriority
 {
     CGFloat horizontalMargin = MARGIN_HORIZONTAL;
     CGFloat rowWidth = WINDOW_WIDTH;
     CGFloat rowHeight = [ExchangeDetailView rowHeight];
     UIView *rowView = [[UIView alloc] initWithFrame:CGRectMake(0, posY, rowWidth, rowHeight)];
 
-    UILabel *mainLabel = [[UILabel alloc] initWithFrame:CGRectMake(horizontalMargin, 0, rowWidth/2, rowHeight)];
+    CGFloat maxLabelWidth = rowWidth/2 - horizontalMargin;
+    UILabel *mainLabel = [[UILabel alloc] initWithFrame:CGRectMake(horizontalMargin, 0, 0, 0)];
     mainLabel.font = [UIFont fontWithName:FONT_MONTSERRAT_REGULAR size:FONT_SIZE_EXTRA_SMALL];
     mainLabel.textColor = COLOR_TEXT_DARK_GRAY;
     mainLabel.text = text;
+    [mainLabel sizeToFit];
+    [mainLabel changeHeight:rowHeight];
     [rowView addSubview:mainLabel];
 
-    UILabel *accessoryLabel = [[UILabel alloc] initWithFrame:CGRectMake(rowWidth/2, 0, rowWidth/2 - horizontalMargin, rowHeight)];
+    CGFloat labelWidthLeeWay = rowWidth/10;
+    if (accessoryLabelWidthHasPriority) {
+        
+        UILabel *testAccessoryLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        testAccessoryLabel.font = [UIFont fontWithName:FONT_MONTSERRAT_REGULAR size:FONT_SIZE_EXTRA_SMALL];
+        testAccessoryLabel.numberOfLines = 0;
+        testAccessoryLabel.text = accessoryText;
+        [testAccessoryLabel sizeToFit];
+        
+        if (testAccessoryLabel.frame.size.width > maxLabelWidth + labelWidthLeeWay){
+            [mainLabel changeWidth:maxLabelWidth - labelWidthLeeWay];
+        } else if (testAccessoryLabel.frame.size.width < maxLabelWidth - labelWidthLeeWay) {
+            [mainLabel changeWidth:maxLabelWidth + labelWidthLeeWay];
+        } else {
+            [mainLabel changeWidth:rowWidth - testAccessoryLabel.frame.size.width - horizontalMargin*2];
+        }
+    } else {
+        if (mainLabel.frame.size.width > maxLabelWidth + labelWidthLeeWay){
+            [mainLabel changeWidth:maxLabelWidth + labelWidthLeeWay];
+        } else if (mainLabel.frame.size.width < maxLabelWidth - labelWidthLeeWay) {
+            [mainLabel changeWidth:maxLabelWidth - labelWidthLeeWay];
+        }
+    }
+    
+    CGFloat accessoryLabelOriginX = mainLabel.frame.origin.x + mainLabel.frame.size.width;
+
+    UILabel *accessoryLabel = [[UILabel alloc] initWithFrame:CGRectMake(accessoryLabelOriginX, 0, self.frame.size.width - accessoryLabelOriginX - horizontalMargin, rowHeight)];
     accessoryLabel.font = [UIFont fontWithName:FONT_MONTSERRAT_REGULAR size:FONT_SIZE_EXTRA_SMALL];
     accessoryLabel.text = accessoryText;
     accessoryLabel.textColor = COLOR_TEXT_DARK_GRAY;
